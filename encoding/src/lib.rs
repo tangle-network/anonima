7c4e2ab01be7e7d7b8937cbaeda22cd928f83890
Drew Stone <drewstone329@gmail.com>
2021-05-02 18:32:07 +0300

    Add encoding/utils

---
+++ encoding/src/lib.rs
@@ -0,0 +1,22 @@
+// Copyright 2020 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+mod bytes;
+mod errors;
+mod hash;
+
+pub use serde::{de, ser};
+pub use serde_bytes;
+pub use serde_cbor::{error, from_reader, from_slice, tags, to_vec, to_writer};
+
+pub use self::bytes::*;
+pub use self::errors::*;
+pub use self::hash::*;
+
+pub mod tuple {
+    pub use serde_tuple::{self, Deserialize_tuple, Serialize_tuple};
+}
+
+pub mod repr {
+    pub use serde_repr::{Deserialize_repr, Serialize_repr};
+}
